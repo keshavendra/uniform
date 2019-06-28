8b3fe7ebfeee9209e027a8200e104b01fb7ad2f3
keshavendra <35345617+keshavendra@users.noreply.github.com>
2019-06-29 00:10:24 +0530

    Added cross origin.
    Added cross origin.

--- src/main/java/com/kps/store/application/controller/VendorController.java
+++ src/main/java/com/kps/store/application/controller/VendorController.java
@@ -3,6 +3,7 @@ package com.kps.store.application.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.kps.store.application.model.VendorModel;
 import com.kps.store.application.service.VendorService;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = { "/vendor", "/VENDOR" })
 public class VendorController {
 	@Autowired
